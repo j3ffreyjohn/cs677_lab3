7a2d3fdf8e46a66cc65b19a2310e7f55ded54963
Jeffrey Geevarghese <j3ffreyjohn@gmail.com>
2013-04-25 21:02:09 -0400

    Testing git Updation

--- bird.py
+++ bird.py
@@ -1,1 +1,8 @@
 #This is the bird file
+
+import sys
+
+N = sys.argv[1]		# N is the number of pigs in the system
+M = sys.argv[2]		# M is the number of bird launches in one game
+
+print 'N = ', N, ' M = ', M
