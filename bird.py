dc00a054541d63b4c38057a25395c7d4c374c9c4
jeffrey <j3ffreyjohn@gmail.com>
2013-04-25 21:29:26 -0400

    Simple changes

--- bird.py
+++ bird.py
@@ -1,8 +1,16 @@
-#This is the bird file
+#This is the bird process which spawns all other processes
 
 import sys
+import random
 
-N = sys.argv[1]		# N is the number of pigs in the system
-M = sys.argv[2]		# M is the number of bird launches in one game
+N = int(sys.argv[1])		# N is the number of pigs in the system
+M = int(sys.argv[2])		# M is the number of bird launches in one game
 
-print 'N = ', N, ' M = ', M
+#Choose two pigs to be the coordinators at random
+coordinators = random.sample(range(1,N+1),2)	
+pigs = list(set(range(1,N+1)) - set(coordinators))
+
+#Assign the rest of the pigs to one of these coordinators
+random.shuffle(pigs)
+pigs_split = [pigs[i::2] for i in range(2)]	#Assign the pigs randomly to one of the coordinators
+
